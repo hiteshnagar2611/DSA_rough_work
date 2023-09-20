d2eddc64a0c3066d0b45cf744195c1722d05ca9e
Hitesh Nagar <74867491+hiteshnagar2611@users.noreply.github.com>
2023-09-20 11:48:53 +0000

    .

--- ssoslab/main.cpp
+++ ssoslab/main.cpp
@@ -4,5 +4,45 @@ using namespace std;
 
 int main(){
 
+    #include<iostream>
+    #include<fstream>
+    #include<string>
+    #include<sstream>
+    #include<vector>
+    #include<map> 
+    using namespace std;
+    int main() {
+
+    ifstream inputFile("input.asm");
+    map symbolTable;
+    vector instructions;
+    string line;
+    while (getline(inputFile, line)) {
+    vector tokens;
+    for (int i = 0; i < line.length(); i++) {
+        if (line[i] == ' ') {
+        tokens.push_back("");
+        } else {
+        tokens.push_back(line.substr(i));
+        break;
+        }
+    }
+
+    if (tokens[0].back() == ':') {
+        symbolTable[tokens[0].substr(0, tokens[0].length() - 1)] = instructions.size();
+    } else {
+        instructions.push_back(line);
+    }
+    }
+
+    inputFile.close();
+    ofstream outputFile("output.txt");
+    for (map::iterator it = symbolTable.begin(); it != symbolTable.end(); it++) {
+    outputFile << it->first << " " << it->second << endl;
+    }
+    for (int i = 0; i < instructions.size(); i++) {
+    outputFile << instructions[i] << endl;
+    }
+    outputFile.close();
     return 0;
 }
