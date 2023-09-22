e8ba247ad29d9a128dd304b3827db12848e82767
Hitesh Nagar <74867491+hiteshnagar2611@users.noreply.github.com>
2023-09-22 12:24:42 +0000

    .

--- first_repiting_element_in_array.cpp
+++ first_repiting_element_in_array.cpp
@@ -1,19 +1,53 @@
-#include <iostream>
+// TIME LIMIT EXTEED
 
+// #include <iostream>
+// using namespace std;
+
+// void findrepiting(int arr[],int n){
+//     for(int i=0;i<n;i++){
+//         bool repiting=false;
+//         for(int j=i+1;j<n;j++){
+//             if(arr[i]==arr[j]){
+//                 cout<<arr[i]<<endl;
+//                 break;
+//             }
+//         }
+//     }
+// }
+
+// int main(){
+//     int n;
+//     int arr[]={1,2,4,5,3,5};
+//     n=sizeof(arr)/sizeof(int);
+
+//     findrepiting(arr,n);
+
+//     return 0;
+
+// }
+
+////////////////////////////////////////////////////////////////////////////
+//USING ANOTHER ARRAY
+#include <iostream>
+//#include <unordered_map>
 using namespace std;
 
-void findrepiting(int arr[],int n){
+void findrepiting(int *arr,int n){
+    int a[6]={0};
+
     for(int i=0;i<n;i++){
-        bool repiting=false;
-        for(int j=i+1;j<n;j++){
-            if(arr[i]==arr[j]){
-                cout<<arr[i]<<endl;
-                break;
-            }
+        a[arr[i]]= 1 ;
+    }
+
+    for(int j=0;j<6;j++){
+        if(a[j]>1){
+            cout<<a[j]<<endl;   
         }
     }
-}
 
+
+
+}
 
 int main(){
     int n;
