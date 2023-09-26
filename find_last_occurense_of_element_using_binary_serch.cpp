6f82b8411be49112ab3f9a42b11ab878eb46be11
Hitesh Nagar <74867491+hiteshnagar2611@users.noreply.github.com>
2023-09-26 03:57:59 +0000

    .

---
+++ find_last_occurense_of_element_using_binary_serch.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void binaryserch(vector<int> &arr, int target){
+        int start=0;
+        int end=arr.size()-1;
+        int mid= start + (end-start)/2 ;
+        int ans=-1;
+        for(int i=0;i<arr.size();i++){
+            if(arr[mid]==target){
+                ans=mid;
+                start=mid+1;
+                
+            }
+            else if(arr[mid] > target){
+                end= mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+            mid= start + (end-start)/2 ;
+        }
+
+        if(ans){
+            cout<<"element's last ocurense"<<ans<<endl;
+        }
+        else
+            cout<<"element is not present"<<end;
+}
+
+
+int main(){
+    vector<int> arr{1,3,7,7,7,7,7,10,14,16};
+
+    binaryserch(arr,7);
+
+    return 0;
+}
